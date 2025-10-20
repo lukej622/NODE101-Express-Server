c13685ac396c6049a1a2f2d158714f898dcd5cb2
Luke Jaumotte <jaumotteluke@gmail.com>
2025-10-20 12:52:58 -0400

    updated file

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 // import files and packages up here
+const express = require('express');
+var morgan = require('morgan');
 
 
 // create your express server below
-var app;
+const app = express();
+app.use(morgan('dev'));
+
+// Imports the json data file
+const topSpots = require('./data.json');
 
 // add your routes and middleware below
+app.get('/', (req, res) => {
+    res.status(200).send('Sanity Check: Server is running!');
+})
+
+// Routes to return the topSpots json data.
+app.get('/data', (req, res) => {
+    res.json(topSpots);
+})
 
 // finally export the express application
 module.exports = app;
